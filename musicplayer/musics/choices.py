3f83f9a0f933d21e2ad56796f9180b055c72548e
Lam Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2019-07-04 13:48:45 +0700

    Model Initial Development

---
+++ musicplayer/musics/choices.py
@@ -0,0 +1,9 @@
+GENRE_CHOICES = (
+    (1, _("Unknown Genre")),
+    (2, _("Rock")),
+    (3, _("Pop")),
+    (4, _("Ballad")),
+    (5, _("EDM")),
+    (6, _("Jazz")),
+    (7, _("Hiphop"))
+)
--- musicplayer/musics/models.py
+++ musicplayer/musics/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from .choices import *
 
-# Create your models here.
+class Music(models.Model):
+    song = models.CharField(max_length=100, default="Unknown Song")
+    artist = models.ManyToManyField(Artist, on_delete=models.CASCADE, null = True, blank = True, default=None)
+    duration = models.IntegerField(default=0)
+    album = models.ManyToManyField(Album, on_delete=models.CASCADE, null = True, blank = True, default=None)
+    genre = models.IntegerField(choices=GENRE_CHOICES, default=1)
+    cover = models.ImageField(upload_to = 'cover/', null = True, blank = True, default=None )
+
+
+class Album(models.Model):
+    album = models.CharField(max_length=100, default="Unknown Album")
+    date = models.DateTimeField(_("Release Date"), auto_now=False, auto_now_add=False, blank=True, null=True, default=None)
+
+
+class Artist(models.Model):
+    name = models.CharField(max_length=100, default="Unknown Artist")
+
+    
