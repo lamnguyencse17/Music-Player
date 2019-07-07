bfed48eb9f80665315543bf8a389516cefc7e0e9
Lam Nguyen <lam.nguyen.khmt2017@hcmut.edu.vn>
2019-07-08 00:57:25 +0700

    API Milestone
    API is almost ready as get_or_create on new artist is implemented
     #TODO:
    Album also needs implementing
    Files upload

--- musicplayer/musics/api.py
+++ musicplayer/musics/api.py
@@ -1,5 +1,6 @@
 from musics.models import *
-from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from rest_framework import status, viewsets, permissions
 from .serializers import MusicSerializer, ArtistSerializer, AlbumSerializer
 
 class MusicViewSet(viewsets.ModelViewSet):
@@ -8,6 +9,17 @@ class MusicViewSet(viewsets.ModelViewSet):
         permissions.AllowAny
     ]
     serializer_class = MusicSerializer
+
+    def create(self, request):
+        serializer = MusicSerializer(data=request.data)
+        for artist in serializer.initial_data.get('artist'):
+            newartist = Artist(name=artist)
+            if not Artist.objects.filter(name=artist):
+                newartist.save()
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'status':'done'})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
 class ArtistViewSet(viewsets.ModelViewSet):
--- musicplayer/musics/serializers.py
+++ musicplayer/musics/serializers.py
@@ -1,12 +1,53 @@
 from rest_framework import serializers
 from musics.models import *
 
+
 class MusicSerializer(serializers.ModelSerializer):
     artist = serializers.SlugRelatedField(many=True, slug_field="name", queryset=Artist.objects.all())
     album = serializers.SlugRelatedField(many=True, slug_field="album", queryset=Album.objects.all())
+    """print(artist)"""
+    """ for art in list(artist):
+            print(artist)
+            if not Artist.objects.filter(name=art):
+                newartist = Artist(name=art)
+                newartist.save() """
+    """ def validate_artist(self, data):
+        print(Artist.objects.filter(name=data))
+        for artist in value:
+            print(artist)
+            if Artist.objects.filter(name=artist) == None:
+                newartist = Artist(name=art)
+                newartist.save()
+        return data """
+    """ def runthispls(self, value):
+        for artist in value:
+            newartist = Artist(name=artist)
+            if Artist.objects.filter(name=artist) == None:
+                newartist.save()
+        print("first") """
+
+    """ def create(self, validated_data):
+        runthispls(validated_data.get('artist'))
+        print("sec")
+        print(Artist.objects.filter(name=validated_data.get('artist')[0]))
+        newsong = Music.objects.create(
+            song=validated_data.get('song'),
+            duration=validated_data.get('duration'),
+            genre=validated_data.get('genre')
+        )
+        newsong.save()
+        for artist in validated_data.get('artist'):
+            newartist = Artist(name=artist)
+            if Artist.objects.filter(name=artist) == None:
+                newartist.save()
+            newsong.artist.add(newartist)
+        return validated_data """
+
     class Meta:
         model = Music
-        exclude = ()
+        fields = ('song', 'artist', 'duration', 'album', 'genre', 'cover')
+
+
 
 
 class ArtistSerializer(serializers.ModelSerializer):
